67e2e4c172efd12047ea892f7a99a609e27ee7b2
VidyaVT <110297379+VidyaVT@users.noreply.github.com>
2022-10-28 14:34:59 +0530

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -4,6 +4,9 @@ import speech_recognition as sr
 import datetime
 import wikipedia
 import webbrowser
+import sys
+from AppOpener import run
+import os
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -45,6 +48,7 @@ def takeinput():
     return query
 
 
+
     
 if __name__ == "__main__":
     salutations()
@@ -60,6 +64,9 @@ if __name__ == "__main__":
 
         elif 'open youtube ' in query:
             webbrowser.open('www.youtube.com') 
+
+        elif 'open notepad' in query:
+            os.system('Notepad')  
         
         elif 'open stackoverflow' in query:
             webbrowser.open('stackoverflow.com')
@@ -70,7 +77,7 @@ if __name__ == "__main__":
         elif 'open reddit' in query:
             webbrowser.open('www.reddit.com')
         
-        elif 'open my playlist' in query:
+        elif 'my playlist' in query:
             webbrowser.open('https://www.youtube.com/watch?v=IuUDRU9-HRk&list=PLP0zeA5un-Eaw83emzhnlfzRm7K1g3iu-&index=1&ab_channel=SiriusXM')
 
         elif 'news updates' in query:
@@ -80,6 +87,9 @@ if __name__ == "__main__":
         elif 'the time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
             speak(f"The Time is {strTime}")
+
+        elif 'exit now' in query:
+            sys.exit()
 
         
         
